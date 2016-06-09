82df0db5175964e8f796fa3e3f522afbf240bd75
ken5scal <kengoscal@gmail.com>
2016-06-10 00:03:42 +0900

    Successfully implemented des encryption

--- InsecureBankv2/app/src/main/java/com/android/insecurebankv2/DoLogin.java
+++ InsecureBankv2/app/src/main/java/com/android/insecurebankv2/DoLogin.java
@@ -32,12 +32,16 @@ import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /*
 The page that accepts new password and passes it on to the change password 
@@ -46,198 +50,214 @@ module. This new password can then be used by the user to log in to the account.
 */
 
 public class DoLogin extends Activity {
-	String responseString = null;
-	//	Stores the username passed by the calling intent
-	String username;
-	//	Stores the password passed by the calling intent
-	String password;
-	String result;
-	String superSecurePassword;
-	String rememberme_username, rememberme_password;
-	public static final String MYPREFS = "mySharedPreferences";
-	String serverip = "";
-	String serverport = "";
-	String protocol = "http://";
-	BufferedReader reader;
-	SharedPreferences serverDetails;
+    String responseString = null;
+    //	Stores the username passed by the calling intent
+    String username;
+    //	Stores the password passed by the calling intent
+    String password;
+    String result;
+    String superSecurePassword;
+    String rememberme_username, rememberme_password;
+    public static final String MYPREFS = "mySharedPreferences";
+    String serverip = "";
+    String serverport = "";
+    String protocol = "http://";
+    BufferedReader reader;
+    SharedPreferences serverDetails;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_do_login);
-		finish();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_do_login);
+        finish();
 
         // Get Server details from Shared Preference file.
-		serverDetails = PreferenceManager.getDefaultSharedPreferences(this);
-		serverip = serverDetails.getString("serverip", null);
-		serverport = serverDetails.getString("serverport", null);
-        if(serverip!=null && serverport!=null){
+        serverDetails = PreferenceManager.getDefaultSharedPreferences(this);
+        serverip = serverDetails.getString("serverip", null);
+        serverport = serverDetails.getString("serverport", null);
+        if (serverip != null && serverport != null) {
 
-		Intent data = getIntent();
-		username = data.getStringExtra("passed_username");
-		password = data.getStringExtra("passed_password");
-		new RequestTask().execute("username");
+            Intent data = getIntent();
+            username = data.getStringExtra("passed_username");
+            password = data.getStringExtra("passed_password");
+            new RequestTask().execute("username");
 
-        }
-        else
-        {
-            Intent setupServerdetails =new Intent(this,FilePrefActivity.class);
+        } else {
+            Intent setupServerdetails = new Intent(this, FilePrefActivity.class);
             startActivity(setupServerdetails);
-            Toast.makeText(this, "Server path/port not set!",Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Server path/port not set!", Toast.LENGTH_LONG).show();
 
         }
-	}
+    }
 
-	class RequestTask extends AsyncTask < String, String, String > {
+    class RequestTask extends AsyncTask<String, String, String> {
 
-		@Override
-		protected String doInBackground(String...params) {
-			try {
-				postData(params[0]);
-			} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException | IOException | JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                postData(params[0]);
+            } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException | IOException | JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-			return null;
-		}
+            return null;
+        }
 
-		protected void onPostExecute(Double result) {}
-		protected void onProgressUpdate(Integer...progress) {}
+        protected void onPostExecute(Double result) {
+        }
 
-		public void postData(String valueIWantToSend) throws ClientProtocolException, IOException, JSONException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+        protected void onProgressUpdate(Integer... progress) {
+        }
+
+        public void postData(String valueIWantToSend) throws ClientProtocolException, IOException, JSONException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 
 
+            // Create a new HttpClient and Post Header
 
-			// Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(protocol + serverip + ":" + serverport + "/login");
+            HttpPost httppost2 = new HttpPost(protocol + serverip + ":" + serverport + "/devlogin");
 
-			HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost(protocol + serverip + ":" + serverport + "/login");
-			HttpPost httppost2 = new HttpPost(protocol + serverip + ":" + serverport + "/devlogin");
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
 
-			// Add your data
-			List < NameValuePair > nameValuePairs = new ArrayList < NameValuePair > (2);
+            //                Delete below test accounts in production
+            //                nameValuePairs.add(new BasicNameValuePair("username", "jack"));
+            //                nameValuePairs.add(new BasicNameValuePair("password", "jack@123$"));
 
-			//                Delete below test accounts in production
-			//                nameValuePairs.add(new BasicNameValuePair("username", "jack"));
-			//                nameValuePairs.add(new BasicNameValuePair("password", "jack@123$"));
+            nameValuePairs.add(new BasicNameValuePair("username", username));
+            nameValuePairs.add(new BasicNameValuePair("password", password));
+            HttpResponse responseBody;
+            if (username.equals("devadmin")) {
+                httppost2.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                // Execute HTTP Post Request
+                responseBody = httpclient.execute(httppost2);
+            } else {
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                // Execute HTTP Post Request
+                responseBody = httpclient.execute(httppost);
+            }
 
-			nameValuePairs.add(new BasicNameValuePair("username", username));
-			nameValuePairs.add(new BasicNameValuePair("password", password));
-			HttpResponse responseBody;
-			if (username.equals("devadmin")) {
-				httppost2.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-				// Execute HTTP Post Request
-				responseBody = httpclient.execute(httppost2);
-			} else {
-				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-				// Execute HTTP Post Request
-				responseBody = httpclient.execute(httppost);
-			}
+            InputStream in = responseBody.getEntity().getContent();
+            result = convertStreamToString(in);
+            result = result.replace("\n", "");
+            if (result != null) {
+                if (result.indexOf("Correct Credentials") != -1) {
+                    Log.v("Log Vervose Login:", ", account=" + username + ":" + password);
+                    saveCreds(username, password);
+                    trackUserLogins();
+                    Intent pL = new Intent(getApplicationContext(), PostLogin.class);
+                    pL.putExtra("uname", username);
+                    startActivity(pL);
+                } else {
+                    Intent xi = new Intent(getApplicationContext(), WrongLogin.class);
+                    startActivity(xi);
+                }
+            }
+        }
 
-			InputStream in = responseBody.getEntity().getContent();
-			result = convertStreamToString( in );
-			result = result.replace("\n", "");
-			if (result != null) {
-				if (result.indexOf("Correct Credentials") != -1) {
-					Log.v("Log Vervose Login:", ", account=" + username + ":" + password);
-					saveCreds(username, password);
-					trackUserLogins();
-					Intent pL = new Intent(getApplicationContext(), PostLogin.class);
-					pL.putExtra("uname", username);
-					startActivity(pL);
-				} else {
-					Intent xi = new Intent(getApplicationContext(), WrongLogin.class);
-					startActivity(xi);
-				}
-			}
-		}
+        /*
+        The function that tracks all the users who have successfully
+        logged in to the application using that device
+        */
+        private void trackUserLogins() {
+            // TODO Auto-generated method stub
+            runOnUiThread(new Runnable() {
 
-		/*
-		The function that tracks all the users who have successfully
-		logged in to the application using that device
-		*/
-		private void trackUserLogins() {
-			// TODO Auto-generated method stub
-			runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // TODO Auto-generated method stub
+                    ContentValues values = new ContentValues();
+                    values.put(TrackUserContentProvider.name, username);
+                    // Inserts content into the Content Provider to track the logged in user's list
+                    Uri uri = getContentResolver().insert(TrackUserContentProvider.CONTENT_URI, values);
 
-				@Override
-				public void run() {
-					// TODO Auto-generated method stub
-					ContentValues values = new ContentValues();
-					values.put(TrackUserContentProvider.name, username);
-					// Inserts content into the Content Provider to track the logged in user's list
-					Uri uri = getContentResolver().insert(TrackUserContentProvider.CONTENT_URI, values);
+                }
+            });
 
-				}
-			});
+        }
 
-		}
+        /*
+        The function that saves the credentials locally for future reference
+        username: username entered by the user
+        password: password entered by the user
+        */
+        private void saveCreds(String username, String password) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+            // TODO Auto-generated method stub
+            SharedPreferences mySharedPreferences;
+            mySharedPreferences = getSharedPreferences(MYPREFS, Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor = mySharedPreferences.edit();
+            rememberme_username = username;
+            rememberme_password = password;
 
-		/*
-		The function that saves the credentials locally for future reference
-		username: username entered by the user
-		password: password entered by the user
-		*/
-		private void saveCreds(String username, String password) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-			// TODO Auto-generated method stub
-			SharedPreferences mySharedPreferences;
-			mySharedPreferences = getSharedPreferences(MYPREFS, Activity.MODE_PRIVATE);
-			SharedPreferences.Editor editor = mySharedPreferences.edit();
-			rememberme_username = username;
-			rememberme_password = password;
-			String base64Username = new String(Base64.encodeToString(rememberme_username.getBytes(), 4));
-			CryptoClass crypt = new CryptoClass();;
-			superSecurePassword = crypt.aesEncryptedString(rememberme_password);
-			editor.putString("EncryptedUsername", base64Username);
-			editor.putString("superSecurePassword", superSecurePassword);
-			editor.commit();
-		}
+            String key = "hogehoge";
+            byte[] keyBytes = key.getBytes("UTF-8");
+            byte[] ivBytes = {
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+            };
+            AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+            SecretKeySpec newKey = new SecretKeySpec(keyBytes, "DES");
+            Cipher cipher = null;
+            cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
+            byte[] encrypted = cipher.doFinal(rememberme_password.getBytes("UTF-8"));
 
-		private String convertStreamToString(InputStream in ) throws IOException {
-			// TODO Auto-generated method stub
-			try {
-				reader = new BufferedReader(new InputStreamReader( in , "UTF-8"));
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			} in .close();
-			return sb.toString();
-		}
-	}
+
+            String base64Username = Base64.encodeToString(encrypted, Base64.DEFAULT);
+            CryptoClass crypt = new CryptoClass();
+            superSecurePassword = crypt.aesEncryptedString(rememberme_password);
+            editor.putString("EncryptedUsername", base64Username);
+            editor.putString("superSecurePassword", superSecurePassword);
+            editor.commit();
+        }
+
+        private String convertStreamToString(InputStream in) throws IOException {
+            // TODO Auto-generated method stub
+            try {
+                reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            in.close();
+            return sb.toString();
+        }
+    }
+
     // Added for handling menu operations
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
     // Added for handling menu operations
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			callPreferences();
-			return true;
-		} else if (id == R.id.action_exit) {
-			Intent i = new Intent(getBaseContext(), LoginActivity.class);
-			i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(i);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            callPreferences();
+            return true;
+        } else if (id == R.id.action_exit) {
+            Intent i = new Intent(getBaseContext(), LoginActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	public void callPreferences() {
-		// TODO Auto-generated method stub
-		Intent i = new Intent(this, FilePrefActivity.class);
-		startActivity(i);
-	}
+    public void callPreferences() {
+        // TODO Auto-generated method stub
+        Intent i = new Intent(this, FilePrefActivity.class);
+        startActivity(i);
+    }
 }
